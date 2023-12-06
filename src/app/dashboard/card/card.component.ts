58e5daee3c193bb5b243a87d6921ba9e2a4457db
damiaofranca <francadamiao202@gmail.com>
2023-12-06 15:36:22 -0300

    feat: update features on application

--- src/app/dashboard/card/card.component.ts
+++ src/app/dashboard/card/card.component.ts
@@ -1,24 +1,74 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Component, OnInit, Input } from "@angular/core";
+import { take, finalize } from "rxjs/operators";
+
+import JSConfetti from "js-confetti";
+
+type Card = any;
 
 @Component({
-  selector: 'app-card',
-  templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+	selector: "app-card",
+	templateUrl: "./card.component.html",
+	styleUrls: ["./card.component.css"],
 })
 export class CardComponent implements OnInit {
+	private _confetti = new JSConfetti();
 
-  @Input() card;
-  constructor() { }
+	private _isLoadingLike = false;
+	public get isLoadingLike() {
+		return this._isLoadingLike;
+	}
 
-  ngOnInit() {
-  }
+	private _occursSomeErrorOnLike = false;
+	public get occursSomeErrorOnLike() {
+		return this._occursSomeErrorOnLike;
+	}
 
-  onLike(card: any){
-    // TODO: incrementar o like, salvar via rest
-  }
+	@Input() card: Card;
 
-  onShare(card: any){
-    // TODO: abrir o link do seu linkedin
-  }
+	constructor(private readonly _http: HttpClient) {}
 
+	ngOnInit() {}
+
+	onLike() {
+		this._isLoadingLike = true;
+		this._http
+			.put<Card>(`/api/skills/${this.card.id}`, {})
+			.pipe(
+				take(1),
+				finalize(() => {
+					this._isLoadingLike = false;
+				}),
+			)
+			.subscribe({
+				next: (card) => {
+					this.card.likes = card.likes;
+					this._occursSomeErrorOnLike = false;
+					this.onSuccessLike();
+				},
+				error: () => {
+					this._occursSomeErrorOnLike = true;
+				},
+			});
+	}
+
+	onSuccessLike() {
+		this._confetti.addConfetti({
+			confettiColors: [
+				"#ff0a54",
+				"#ff477e",
+				"#ff7096",
+				"#ff85a1",
+				"#fbb1bd",
+				"#f9bec7",
+			],
+		});
+	}
+
+	onShare() {
+		window.open(
+			"https://www.linkedin.com/in/dami%C3%A3o-fran%C3%A7a-a47b20223/",
+			"_blank",
+		);
+	}
 }
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,19 +1,57 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+import { take, finalize } from "rxjs/operators";
+import { BehaviorSubject } from "rxjs";
+
+type Cards = any[];
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+	selector: "app-dashboard",
+	templateUrl: "./dashboard.component.html",
+	styleUrls: ["./dashboard.component.css"],
 })
 export class DashboardComponent implements OnInit {
+	private readonly _cards$ = new BehaviorSubject<Cards>([]);
+	public readonly cards$ = this._cards$.asObservable();
 
-  cards: Array<any>;
+	private _isLoading = false;
+	public get isLoading() {
+		return this._isLoading;
+	}
 
-  constructor(private httpClient: HttpClient) { }
+	private _occursSomeError = false;
+	public get occursSomeError() {
+		return this._occursSomeError;
+	}
 
-  ngOnInit() {
-    this.httpClient.get('/api/skills').subscribe((ret: Array<any>) => this.cards = ret);
-  }
+	constructor(private httpClient: HttpClient) {}
 
+	public loadCards(): void {
+		this._isLoading = true;
+
+		this.httpClient
+			.get<Cards>("/api/skills")
+			.pipe(
+				take(1),
+				finalize(() => {
+					this._isLoading = false;
+				}),
+			)
+			.subscribe({
+				next: (response) => {
+					this._cards$.next(response);
+					this._occursSomeError = false;
+					//Se quiser ver o erro caso a request falhe, atribuia como true o this._occursSomeError.
+				},
+				// Não implementado tratamento de erros pois, pois estamos desenvolvendo uma aplicação de testes
+				error: () => {
+					this._occursSomeError = true;
+				},
+			});
+	}
+
+	ngOnInit() {
+		this.loadCards();
+	}
 }
